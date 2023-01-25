26feec9115cde066892559307d2360be24b2ab50
Gustavo <contato@gustavomaia.com>
2023-01-25 18:08:54 -0300

    url controllers statuses

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,23 +2,27 @@ class UrlsController < ApplicationController
 
   def index
     urls = Url.all.select(:id, :address, :key)
-    render json: urls
+    render json: urls, status: :ok
   end
 
   def create
     url = Url.create(address: params[:address])
     
     if url.save
-      render json: url
+      render json: url, status: :created
     else
-      render json: url.errors.full_messages
+      render json: url.errors.full_messages, status: :unprocessable_entity
     end
   end
 
   def show
     url = Url.find_by_key(params[:key])
     
-    render json: url
+    if url
+      render json: url, status: :found
+    else
+      render json: url, status: :not_found
+    end
   end
 
   def url_params
