56e8fa11197b7aec8f9732fea31b5812ac6670fb
Gustavo <contato@gustavomaia.com>
2023-01-25 16:10:44 -0300

    urls controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,17 @@
 class UrlsController < ApplicationController
+
+  def index
+    urls = Url.all.select(:id, :address, :key)
+    render json: urls
+  end
+
+  def create
+    url = Url.create(address: params[:address])
+    
+    render json: url
+  end
+
+  def url_params
+    params.permit(:address)
+  end
 end
